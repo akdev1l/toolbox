981b43a285ab6b28c9c25c82a41d60c18ea2fd88
Alex Diaz <alex@akdev.xyz>
2022-08-26 08:01:30 -0400

    fix: relocation bug

--- ci/util/publish.sh
+++ ci/util/publish.sh
@@ -2,7 +2,7 @@
 set -x
 
 main() {
-    image_name="$(./util/build.sh "${1}" | awk '{print $2}')"
+    image_name="$(./ci/util/build.sh "${1}" | awk '{print $2}')"
 
     podman tag "localhost/${image_name}" "${TOOLBOX_REPO}/${image_name}"
     podman push "${image_name}" "${TOOLBOX_REPO}/${image_name}"
